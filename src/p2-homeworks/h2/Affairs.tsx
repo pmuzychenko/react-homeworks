9495143a2f92e8dc90498d7914c5147d7f038d08
pmuzychenko <pmuzychenko@gmail.com>
2020-11-22 13:06:16 +0200

    fix: change styles for SuperButton component and App component

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Affair from "./Affair";
 import {AffairType, FilterType} from "./HW2";
+import styles from "./Affairs.module.css"
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 type AffairsPropsType = { // need to fix any
     data: Array<AffairType>
@@ -23,14 +25,13 @@ function Affairs(props: AffairsPropsType) {
     const setLow = () => {props.setFilter('low')};
 
     return (
-        <div>
+        <div className={styles.affairsButtons}>
 
             {mappedAffairs}
-
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <SuperButton onClick={setAll}>All</SuperButton>
+            <SuperButton onClick={setHigh}>High</SuperButton>
+            <SuperButton onClick={setMiddle}>Middle</SuperButton>
+            <SuperButton onClick={setLow}>Low</SuperButton>
         </div>
     );
 }
